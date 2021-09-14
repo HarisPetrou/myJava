05c697016c9b4c367ec1c16dae810926e71a7bf6
HarisPetrou <59084378+HarisPetrou@users.noreply.github.com>
2021-09-14 09:43:24 +0300

    Delete Spring-Hibernate/src/gr/hua/dit/spr_hib directory

--- Spring-Hibernate/src/gr/hua/dit/spr_hib/security/SecurityWebApplicationInitializer.java
+++
@@ -1,7 +0,0 @@
-package gr.hua.dit.spr_hib.security;
-
-import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
-
-public class SecurityWebApplicationInitializer extends AbstractSecurityWebApplicationInitializer{
-
-}
